0ba84d73f382011659e6e3102c03f0bb1897d506
Hutasako <ejjungao@yahoo.com>
2021-08-13 01:24:14 -0400

    added pagination(i think)

--- src/components/products.js
+++ src/components/products.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState} from 'react'
 import SingleProduct from './products_single'
+import {Pagination} from '@material-ui/lab'
 
 const Products = ({data}) => {
     console.log(`Printing`)
     console.log(`${data}`)
 
-    const ProductsDisplay = data.map((product) => <SingleProduct key={product.id} data={product}/>)
+    const [currentPage, setCurrentPage] = useState(1)
+    const pageSize = 5
 
+    const handleChange = (event,value) => {
+        setCurrentPage(value)
+    }
+    
+    const start = (currentPage - 1) * pageSize
+    const end = start + pageSize
+
+    const numberOfPages = Math.ceil(data.length/pageSize)
+
+    const ProductsDisplay = data.slice(start, end).map((product) => <SingleProduct key={product.id} data={product}/>)
+    console.log("Products!")
+    console.log(`${currentPage}`)
+    console.log(ProductsDisplay)
     return (
         <section className="results">
             <h2 className="subheading">
                 Results
             </h2>
             {ProductsDisplay}
+            <div className="pagination-wrapper">
+                <Pagination  count={numberOfPages} onChange={handleChange}/>
+            </div>
         </section>
     )
 }
--- src/components/ratings.js
+++ src/components/ratings.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Star, StarOutline} from '@material-ui/icons'
 
 const Ratings = ({data}) => {
 
@@ -6,25 +7,29 @@ const Ratings = ({data}) => {
     console.log(`Ratings: ${ratings}`)
     const starContainer = document.createElement('dd');
     let stars = ratings;
-
+    let starsArr = []
     // Print filled stars
     for (let i = 0;i < ratings; i++){
-        stars += `<span class="material-icons">star</span>`;
+        // stars += `<span class="material-icons">star</span>`;
+        starsArr.push(<Star/>);
     }
 
     // Print empty stars if rating < 5
     if (ratings < 5){
         let diff = 5 - ratings;
         for (let i = 0;i < diff; i++){
-            stars += `<span class="material-icons">star_border</span>`;
+            // stars += `<span class="material-icons">star_border</span>`;
+            // stars+=<StarOutlineIcon/>;
+            starsArr.push(<StarOutline/>)
         }
     }
     starContainer.innerHTML = stars;
     console.log(`Ratings: ${stars}`);
     return (
-        <>
-        {stars}
-        </>
+        <p>
+            {stars}
+            {starsArr}
+        </p>
     )
 }
 
